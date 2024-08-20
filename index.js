bbc2770c76fb77bb2f174dc36cd55fd8ab661ebc
Boluwatife Olaoye <Olaoyeboluwatife20@gmail.com>
2024-08-20 09:56:28 +0100

    Added a file

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+
+//create server
+const PORT = 2000
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    let data =[
+        {name:"Bolu", age: 13, height: "6 ft"},
+        {name:"Mide", age: 10, height: "4 ft"},
+        {name:"Priscy", age: 12, height: "5 ft"},
+        
+    ]
+res.render("Home",{data:data})
+})
+
+
+app.get("/user",(req,res)=>{
+    fetch("https://api.github.com/users")
+    .then((response)=>response.json())
+    .then((data)=>{
+        res.render("github", {data:data})
+    })
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log("Running o");
+    
+})
